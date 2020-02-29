6b4b2da2a3d920ce0164245a03a9372cd98b497d
Jozef Kudrys <jskudrys@gmail.com>
2020-02-29 14:00:25 +0100

    Added "Executioner" class
    Executioner class provides several functions to do with the prisioner

--- src/Workers/Executioner.java
+++ src/Workers/Executioner.java
@@ -1,5 +1,15 @@
 package Workers;
 
 public class Executioner {
+    public void executePrisioner() {
+        System.out.println("Prisioner has been executed with a toy sword...");
+    }
 
+    public void torture() {
+        System.out.println("Start tickling...");
+    }
+
+    public void release() {
+        System.out.println("The prisioner has been released...");
+    }
 }
