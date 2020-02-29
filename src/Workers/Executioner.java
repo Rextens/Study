1da1136768d8703bce92fed4abcd66dfd8956e3b
Jozef Kudrys <jskudrys@gmail.com>
2020-02-29 14:02:45 +0100

    Added Executioner variable system
    Variable system provides informations about prisioner

--- src/Workers/Executioner.java
+++ src/Workers/Executioner.java
@@ -1,5 +1,7 @@
 package Workers;
 
 public class Executioner {
-
+    public boolean hasBeenExecutedWithAToySword = false;
+    public boolean hasBeenTickled = false;
+    public boolean isFree = true;
 }
